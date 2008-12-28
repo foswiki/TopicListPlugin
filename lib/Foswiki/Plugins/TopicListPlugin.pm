6dd1b53850500c5827894e5abf5a22743571f241
EugenMayer <EugenMayer@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-28 23:18:29 +0000

    Item593: fixed minor bugs, added delimiter / globalformat, cosmetic things, new defaults
    git-svn-id: http://svn.foswiki.org/trunk/TopicListPlugin@1637 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicListPlugin.pm
+++ lib/Foswiki/Plugins/TopicListPlugin.pm
@@ -36,7 +36,7 @@ $VERSION = '$Rev: 12445$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '0.2';
+$RELEASE = '0.3';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
@@ -53,6 +53,7 @@ sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
     Foswiki::Func::registerRESTHandler('topiclist', \&_topicList);
+    Foswiki::Func::registerRESTHandler('weblist', \&_webList);
     Foswiki::Func::registerTagHandler( 'GETTOPICLIST',\&_macrotopicList );
     
     return 1;
@@ -60,35 +61,46 @@ sub initPlugin {
 
 sub _macrotopicList{
 	my ( $this, $params, $topic, $web ) = @_;
-    my $searchWebs = $params->{"SEARCHWEBS"} || $web;
+    my $searchWebs = $params->{"searchwebs"} || $web;
     my $pattern = $params->{"pattern"} || '*';
     my $casesensitive = $params->{"casesens"};
     $casesensitive = 1 if !defined $casesensitive;
     my $negate = $params->{"negate"} || 0;
-    my $max = $params->{"max"} || 50;
+    my $max = $params->{"max"};
+    $max = 50 if !defined $max;
     my $order = $params->{"order"} || '<';
-    my $format = $params->{"format"} || "   * %WEB.%TOPIC\n";
+    my $format = _expandString($params->{"format"}) || "   * %WEB.%TOPIC";
+    my $delimiter = $params->{"delimiter"};
+    $delimiter = "\n" if !defined $delimiter;
+    $delimiter = _expandString($delimiter);
     
-    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format);
+    my $globalFormat = _expandString($params->{"globalformat"}) || '%TOPICS';
+    
+    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,,$delimiter,$globalFormat);
 }
 sub _topicList{
     my $session = shift;    
     my $web = $session->{webName};
     my $topic = $session->{topicName};  
     my $query = $session->{cgiQuery};
-    my $searchWebs = $query->param("SEARCHWEBS") || $web;
+    my $searchWebs = $query->param("searchwebs") || $web;
     my $pattern = $query->param("pattern") || '*';
     my $casesensitive = $query->param("casesens") || 1;
     my $negate = $query->param("negate") || 0;
-    my $max = $query->param("max") || 50;
+    my $max = $query->param("max");
+    $max = 50 if !defined $max;
     my $order = $query->param("order") || '<';
-    my $format = $query->param("format") || "%WEB.%TOPIC\n";
- 
-    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format);
+    my $format = _expandString($query->param("format")) || "'%WEB.%TOPIC'";
+    my $delimiter = $query->param("delimiter");
+    $delimiter = "," if !defined $delimiter;
+    $delimiter = _expandString($delimiter);
+    my $globalFormat = _expandString($query->param("globalformat")) || '[%TOPICS]';
+    
+    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat);
 }
 
 sub _getTopicList {
-    my ($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format) = @_;
+    my ($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat) = @_;
 
     my @webs = [];
     if($searchWebs eq "-all-") {
@@ -108,7 +120,7 @@ sub _getTopicList {
     	$topics{$web} = _getTopicsOfWebByFunc($web,$pattern, $order, $max, $casesensitive, $negate);  	
     }
     
-    return _getOutput($format,\%topics);
+    return _getOutput($format,$delimiter,$globalFormat,\%topics);
 }
 
 sub _getTopicsOfWebByFunc {
@@ -122,8 +134,16 @@ sub _getTopicsOfWebByFunc {
     _debug("max : $max");
     $refTopics = _filterTopicList($pattern,$casesensitive,$negate,$refTopics);
     
-    my @topcis = splice @{$refTopics}, 0, $max;
-    _debug("Topics spliced",@topcis);
+    
+    my @topcis;
+    if($max > 0) {
+      splice @{$refTopics}, 0, $max;    
+      _debug("Topics spliced",@topcis);      
+     }
+     else {
+     	@topcis = @{$refTopics};
+     }
+    
 
     # as topics are read of from the FS, they normaly are in < order ( literally ), so if we sort > , flip the array 
     @topcis =  reverse @topcis if($order eq ">");
@@ -162,21 +182,26 @@ sub _filterTopicList {
 }
 
 sub _getOutput {
-	my ($format, $refWebTopicPairs) = @_;
+	my ($format,$delimiter,$globalFormat, $refWebTopicPairs) = @_;
 	my $output = "";
+
 	foreach my $web ( keys %{$refWebTopicPairs}) {
 		foreach my $topicName (@{$refWebTopicPairs->{$web}}) {
 			my $tmp = $format;
 			$tmp =~ s/%WEB/$web/g;
 			$tmp =~ s/%TOPIC/$topicName/g;
-			$output .= $tmp;			
+			$output .= $tmp.$delimiter;			
 		}		
 	}
-	return $output;	
+	# remove the laste delimiter
+	$output = substr($output,0,-length($delimiter));	
+	$globalFormat =~ s/%TOPICS/$output/g;	
+	
+	return $globalFormat;	
 } 
 
 sub _debug {
-    return if 1; #!$Foswiki::cfg{Plugins}{TopicListPlugin}{Debug};
+    return if !0; #!$Foswiki::cfg{Plugins}{TopicListPlugin}{Debug};
     my ( $message, @param ) = @_;
 
     Foswiki::Func::writeDebug( "[TopicListPlugin]:" . $message );
@@ -195,4 +220,29 @@ sub _warn {
 }
 1;
 
+sub _webList {
+	my $session = shift;  
+    my $web = $session->{webName};
+    my $topic = $session->{topicName};  
+    my $query = $session->{cgiQuery};
+    my $exculdeWebs = $query->param("excludewebs") || "";  
+      
+	my $output = '';
+	my @webs = 	Foswiki::Func::getListOfWebs();
+	
+	foreach my $web (@webs) {
+	   next if($exculdeWebs =~ /$web/);	
+	   $output	.= "\"$web\",";
+	}
+	# remove the laste ,
+	$output = substr($output,0,-1) if $output ne '';
+	
+	return "[$output]";  
+}
+
+sub _expandString {
+	my $string = shift;
+    $string =~ s/%NL/\n/g;        
+	return $string;
+}
 # vim: ft=perl foldmethod=marker
