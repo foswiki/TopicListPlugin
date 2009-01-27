af02cd6988123d8edb90d90fd2a8d812bb803a5c
EugenMayer <EugenMayer@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-27 18:54:20 +0000

    Item876: fixing bug in the macro
    git-svn-id: http://svn.foswiki.org/trunk/TopicListPlugin@2193 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicListPlugin.pm
+++ lib/Foswiki/Plugins/TopicListPlugin.pm
@@ -36,7 +36,7 @@ $VERSION = '$Rev: 12445$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '0.3';
+$RELEASE = '0.4';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
@@ -76,7 +76,7 @@ sub _macrotopicList{
     
     my $globalFormat = _expandString($params->{"globalformat"}) || '%TOPICS';
     
-    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,,$delimiter,$globalFormat);
+    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat);
 }
 sub _topicList{
     my $session = shift;    
