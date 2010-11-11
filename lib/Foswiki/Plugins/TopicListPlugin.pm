4d6fe3c03c2d79ca5defa6682e4e994f7e90b956
PadraigLennon <PadraigLennon@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-11-11 00:29:32 +0000

    Item9995: Fix Release Version
    git-svn-id: http://svn.foswiki.org/trunk/TopicListPlugin@9954 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicListPlugin.pm
+++ lib/Foswiki/Plugins/TopicListPlugin.pm
@@ -36,7 +36,7 @@ $VERSION = '$Rev: 12445$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '0.4';
+$RELEASE = '0.4.1';
 
 # Short description of this plugin
 # One line description, is shown in the %FoswikiWEB%.TextFormattingRules topic:
@@ -245,4 +245,3 @@ sub _expandString {
     $string =~ s/%NL/\n/g;        
 	return $string;
 }
-# vim: ft=perl foldmethod=marker
