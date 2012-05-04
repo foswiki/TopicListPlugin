267fef9ee04b6ec2b3c4a462816f3956f11c6b8b
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/TopicListPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicListPlugin.pm
+++ lib/Foswiki/Plugins/TopicListPlugin.pm
@@ -48,160 +48,181 @@ $pluginName = 'TopicListPlugin';
 
 # =========================
 
-
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    Foswiki::Func::registerRESTHandler('topiclist', \&_topicList);
-    Foswiki::Func::registerRESTHandler('weblist', \&_webList);
-    Foswiki::Func::registerTagHandler( 'GETTOPICLIST',\&_macrotopicList );
-    
+    Foswiki::Func::registerRESTHandler( 'topiclist', \&_topicList );
+    Foswiki::Func::registerRESTHandler( 'weblist',   \&_webList );
+    Foswiki::Func::registerTagHandler( 'GETTOPICLIST', \&_macrotopicList );
+
     return 1;
 }
 
-sub _macrotopicList{
-	my ( $this, $params, $topic, $web ) = @_;
+sub _macrotopicList {
+    my ( $this, $params, $topic, $web ) = @_;
     my $searchWebs = $params->{"searchwebs"} || $web;
-    my $pattern = $params->{"pattern"} || '*';
+    my $pattern    = $params->{"pattern"}    || '*';
     my $casesensitive = $params->{"casesens"};
     $casesensitive = 1 if !defined $casesensitive;
     my $negate = $params->{"negate"} || 0;
     my $max = $params->{"max"};
     $max = 50 if !defined $max;
     my $order = $params->{"order"} || '<';
-    my $format = _expandString($params->{"format"}) || "   * %WEB.%TOPIC";
+    my $format = _expandString( $params->{"format"} ) || "   * %WEB.%TOPIC";
     my $delimiter = $params->{"delimiter"};
     $delimiter = "\n" if !defined $delimiter;
     $delimiter = _expandString($delimiter);
-    
-    my $globalFormat = _expandString($params->{"globalformat"}) || '%TOPICS';
-    
-    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat);
+
+    my $globalFormat = _expandString( $params->{"globalformat"} ) || '%TOPICS';
+
+    return _getTopicList(
+        $searchWebs, $pattern,   $casesensitive,
+        $negate,     $max,       $order,
+        $format,     $delimiter, $globalFormat
+    );
 }
-sub _topicList{
-    my $session = shift;    
-    my $web = $session->{webName};
-    my $topic = $session->{topicName};  
-    my $query = $session->{cgiQuery};
-    my $searchWebs = $query->param("searchwebs") || $web;
-    my $pattern = $query->param("pattern") || '*';
+
+sub _topicList {
+    my $session       = shift;
+    my $web           = $session->{webName};
+    my $topic         = $session->{topicName};
+    my $query         = $session->{cgiQuery};
+    my $searchWebs    = $query->param("searchwebs") || $web;
+    my $pattern       = $query->param("pattern") || '*';
     my $casesensitive = $query->param("casesens") || 1;
-    my $negate = $query->param("negate") || 0;
-    my $max = $query->param("max");
+    my $negate        = $query->param("negate") || 0;
+    my $max           = $query->param("max");
     $max = 50 if !defined $max;
     my $order = $query->param("order") || '<';
-    my $format = _expandString($query->param("format")) || "'%WEB.%TOPIC'";
+    my $format = _expandString( $query->param("format") ) || "'%WEB.%TOPIC'";
     my $delimiter = $query->param("delimiter");
     $delimiter = "," if !defined $delimiter;
     $delimiter = _expandString($delimiter);
-    my $globalFormat = _expandString($query->param("globalformat")) || '[%TOPICS]';
-    
-    return _getTopicList($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat);
+    my $globalFormat = _expandString( $query->param("globalformat") )
+      || '[%TOPICS]';
+
+    return _getTopicList(
+        $searchWebs, $pattern,   $casesensitive,
+        $negate,     $max,       $order,
+        $format,     $delimiter, $globalFormat
+    );
 }
 
 sub _getTopicList {
-    my ($searchWebs,$pattern,$casesensitive,$negate,$max,$order,$format,$delimiter,$globalFormat) = @_;
+    my (
+        $searchWebs, $pattern,   $casesensitive,
+        $negate,     $max,       $order,
+        $format,     $delimiter, $globalFormat
+    ) = @_;
 
     my @webs = [];
-    if($searchWebs eq "-all-") {
+    if ( $searchWebs eq "-all-" ) {
         @webs = Foswiki::Func::getListOfWebs();
     }
     else {
-    	@webs = split (",",$searchWebs);
-    }	
-    
+        @webs = split( ",", $searchWebs );
+    }
+
     my %topics = ();
     foreach my $web (@webs) {
-    	# if this web does not exist, take the next one
-    	if(!Foswiki::Func::webExists($web)) {
-    		next;
-    	}
-    	   
-    	$topics{$web} = _getTopicsOfWebByFunc($web,$pattern, $order, $max, $casesensitive, $negate);  	
+
+        # if this web does not exist, take the next one
+        if ( !Foswiki::Func::webExists($web) ) {
+            next;
+        }
+
+        $topics{$web} =
+          _getTopicsOfWebByFunc( $web, $pattern, $order, $max, $casesensitive,
+            $negate );
     }
-    
-    return _getOutput($format,$delimiter,$globalFormat,\%topics);
+
+    return _getOutput( $format, $delimiter, $globalFormat, \%topics );
 }
 
 sub _getTopicsOfWebByFunc {
-	my ($web,$pattern, $order, $max, $casesensitive, $negate) = @_;
-    
-    # filter by pattern and crop to the maximum size   
-    _debug("web : $web");    
+    my ( $web, $pattern, $order, $max, $casesensitive, $negate ) = @_;
+
+    # filter by pattern and crop to the maximum size
+    _debug("web : $web");
     my @tmp = Foswiki::Func::getTopicList($web);
-    _debug("Topics found",@tmp);
+    _debug( "Topics found", @tmp );
     my $refTopics = \@tmp;
     _debug("max : $max");
-    $refTopics = _filterTopicList($pattern,$casesensitive,$negate,$refTopics);
-    
-    
-    my @topcis;
-    if($max > 0) {
-      splice @{$refTopics}, 0, $max;    
-      _debug("Topics spliced",@topcis);      
-     }
-     else {
-     	@topcis = @{$refTopics};
-     }
-    
+    $refTopics =
+      _filterTopicList( $pattern, $casesensitive, $negate, $refTopics );
 
-    # as topics are read of from the FS, they normaly are in < order ( literally ), so if we sort > , flip the array 
-    @topcis =  reverse @topcis if($order eq ">");
-    
+    my @topcis;
+    if ( $max > 0 ) {
+        splice @{$refTopics}, 0, $max;
+        _debug( "Topics spliced", @topcis );
+    }
+    else {
+        @topcis = @{$refTopics};
+    }
+
+# as topics are read of from the FS, they normaly are in < order ( literally ), so if we sort > , flip the array
+    @topcis = reverse @topcis if ( $order eq ">" );
+
     return \@topcis;
 }
 
 sub _filterTopicList {
-	my ($pattern,$casesensitive,$negate, $refTopics) = @_;
-    return $refTopics if($pattern eq "*" || $pattern eq ".*");
-    
-    _debug("casesense",($casesensitive )); 
-    
-    my @result; 	    
-	for(my $i = 0;$i < @{$refTopics};$i++) {
-		# TODO: this is oOoO ugly!
-		if($negate) {
-			if($casesensitive) {
-			     push (@result,$refTopics->[$i])  if ( $refTopics->[$i] !~ /$pattern/ );
-			}
-			else {
-				 push (@result,$refTopics->[$i])  if ( $refTopics->[$i] !~ /$pattern/i );
-			}
-		}
-		else {
-			 if($casesensitive) {
-                 push (@result,$refTopics->[$i])  if ( $refTopics->[$i] =~  /$pattern/ );
+    my ( $pattern, $casesensitive, $negate, $refTopics ) = @_;
+    return $refTopics if ( $pattern eq "*" || $pattern eq ".*" );
+
+    _debug( "casesense", ($casesensitive) );
+
+    my @result;
+    for ( my $i = 0 ; $i < @{$refTopics} ; $i++ ) {
+
+        # TODO: this is oOoO ugly!
+        if ($negate) {
+            if ($casesensitive) {
+                push( @result, $refTopics->[$i] )
+                  if ( $refTopics->[$i] !~ /$pattern/ );
             }
             else {
-                 push (@result,$refTopics->[$i])  if ( $refTopics->[$i] =~  /$pattern/i );
+                push( @result, $refTopics->[$i] )
+                  if ( $refTopics->[$i] !~ /$pattern/i );
             }
-		}		
-	}
-	_debug("Topics filtered",@result);
-	return \@result;
+        }
+        else {
+            if ($casesensitive) {
+                push( @result, $refTopics->[$i] )
+                  if ( $refTopics->[$i] =~ /$pattern/ );
+            }
+            else {
+                push( @result, $refTopics->[$i] )
+                  if ( $refTopics->[$i] =~ /$pattern/i );
+            }
+        }
+    }
+    _debug( "Topics filtered", @result );
+    return \@result;
 }
 
 sub _getOutput {
-	my ($format,$delimiter,$globalFormat, $refWebTopicPairs) = @_;
-	my $output = "";
+    my ( $format, $delimiter, $globalFormat, $refWebTopicPairs ) = @_;
+    my $output = "";
 
-	foreach my $web ( keys %{$refWebTopicPairs}) {
-		foreach my $topicName (@{$refWebTopicPairs->{$web}}) {
-			my $tmp = $format;
-			$tmp =~ s/%WEB/$web/g;
-			$tmp =~ s/%TOPIC/$topicName/g;
-			$output .= $tmp.$delimiter;			
-		}		
-	}
-	# remove the laste delimiter
-	$output = substr($output,0,-length($delimiter));	
-	$globalFormat =~ s/%TOPICS/$output/g;	
-	
-	return $globalFormat;	
-} 
+    foreach my $web ( keys %{$refWebTopicPairs} ) {
+        foreach my $topicName ( @{ $refWebTopicPairs->{$web} } ) {
+            my $tmp = $format;
+            $tmp =~ s/%WEB/$web/g;
+            $tmp =~ s/%TOPIC/$topicName/g;
+            $output .= $tmp . $delimiter;
+        }
+    }
+
+    # remove the laste delimiter
+    $output = substr( $output, 0, -length($delimiter) );
+    $globalFormat =~ s/%TOPICS/$output/g;
+
+    return $globalFormat;
+}
 
 sub _debug {
-    return if !0; #!$Foswiki::cfg{Plugins}{TopicListPlugin}{Debug};
+    return if !0;    #!$Foswiki::cfg{Plugins}{TopicListPlugin}{Debug};
     my ( $message, @param ) = @_;
 
     Foswiki::Func::writeDebug( "[TopicListPlugin]:" . $message );
@@ -221,27 +242,28 @@ sub _warn {
 1;
 
 sub _webList {
-	my $session = shift;  
-    my $web = $session->{webName};
-    my $topic = $session->{topicName};  
-    my $query = $session->{cgiQuery};
-    my $exculdeWebs = $query->param("excludewebs") || "";  
-      
-	my $output = '';
-	my @webs = 	Foswiki::Func::getListOfWebs();
-	
-	foreach my $web (@webs) {
-	   next if($exculdeWebs =~ /$web/);	
-	   $output	.= "\"$web\",";
-	}
-	# remove the laste ,
-	$output = substr($output,0,-1) if $output ne '';
-	
-	return "[$output]";  
+    my $session     = shift;
+    my $web         = $session->{webName};
+    my $topic       = $session->{topicName};
+    my $query       = $session->{cgiQuery};
+    my $exculdeWebs = $query->param("excludewebs") || "";
+
+    my $output = '';
+    my @webs   = Foswiki::Func::getListOfWebs();
+
+    foreach my $web (@webs) {
+        next if ( $exculdeWebs =~ /$web/ );
+        $output .= "\"$web\",";
+    }
+
+    # remove the laste ,
+    $output = substr( $output, 0, -1 ) if $output ne '';
+
+    return "[$output]";
 }
 
 sub _expandString {
-	my $string = shift;
-    $string =~ s/%NL/\n/g;        
-	return $string;
+    my $string = shift;
+    $string =~ s/%NL/\n/g;
+    return $string;
 }
